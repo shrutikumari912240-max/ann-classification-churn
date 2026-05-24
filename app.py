5d966cbab02f13c5499573d82c3682e41b4b7a90
shrutikumari912240-max <shrutikumari912240@gmail.com>
2026-05-24 16:49:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import keras
 from tensorflow import keras
 from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 import pandas as pd
